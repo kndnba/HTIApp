49df2e329451cc1922d6cfaff076dab140ab091d
kndnba <dedmombed@gmail.com>
2022-11-23 20:39:07 +0600

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/htiapp/activities/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.htiapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bignerdranch.android.htiapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
